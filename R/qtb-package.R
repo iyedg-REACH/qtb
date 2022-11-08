34c72e23400f760e966e72cc42bf3d376a7b2ddd
Iyed GHEDAMSI <iyed.ghedamsi@reach-initiative.org>
2022-11-08 22:03:31 +0100

    define package imports

---
+++ R/qtb-package.R
@@ -0,0 +1,10 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom rlang .data `:=`
+#' @importFrom dplyr select mutate group_by pull filter
+#' @importFrom tidyr pivot_longer pivot_wider
+#' @importFrom tibble tribble
+## usethis namespace: end
+NULL
