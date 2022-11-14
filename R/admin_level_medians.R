23a6a41016e8b71b369158ea2ac8be95238dfffb
Iyed GHEDAMSI <iyed.ghedamsi@reach-initiative.org>
2022-11-14 14:38:11 +0100

    examples for admin_level_medians

--- R/admin_level_medians.R
+++ R/admin_level_medians.R
@@ -4,12 +4,19 @@
 #' @param admin_level_col a column name indicating a Libyan Administrative Level
 #'
 #' @return A dataframe with median prices for each item in the MEB per Administrative Level
+#'
 #' @export
+#'
+#' @examples
+#' admin_level_medians(jmmi_2022_feb, q_region)
+#' admin_level_medians(jmmi_2022_feb, q_district)
+#' admin_level_medians(jmmi_2022_feb, q_municipality)
 admin_level_medians <- function(df,
                                 admin_level_col) {
   df |>
     select({{ admin_level_col }}, pull(meb_weights, .data[["item"]])) |>
     pivot_longer(-{{ admin_level_col }}, names_to = "item", values_to = "price") |>
     group_by({{ admin_level_col }}, .data[["item"]]) |>
-    dplyr::summarise(median_item_price = median(.data[["price"]], na.rm = TRUE))
+    dplyr::summarise(median_item_price = median(.data[["price"]], na.rm = TRUE)) |>
+    dplyr::ungroup()
 }
