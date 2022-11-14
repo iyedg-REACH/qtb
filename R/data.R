96737f7e9155ce608cf9e2dba0a3c64c01fddd95
Iyed GHEDAMSI <iyed.ghedamsi@reach-initiative.org>
2022-11-14 10:31:14 +0100

    MEB weights as package data

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' MEB weights for the Libyan JMMI
+#'
+#' Weights of MEB items used for the cost of the MEB
+#' in Libya
+#'
+#' \describe{
+#'   \item{item}{Item label as per the standard KoBo tool used for data collection}
+#'   \item{group}{The item group of the item}
+#'   \item{label}{A readable label for the item, including the unit}
+#'   \item{weight}{The weight contribution of the item towards the final cost of MEB}
+#' }
+#'
+#' @docType data
+#'
+#' @keywords datasets
+"meb_weights"
---
+++ data-raw/meb_weights.R
@@ -0,0 +1,8 @@
+## code to prepare `meb_weights` dataset goes here
+
+meb_weights <- readr::read_csv(
+  here::here("data-raw/meb_weights.csv"),
+  show_col_types = FALSE
+)
+
+usethis::use_data(meb_weights, overwrite = TRUE)
