6524bec2b95c88f218bba52b0c0dc7761c1818ee
Iyed GHEDAMSI <iyed.ghedamsi@reach-initiative.org>
2022-11-14 13:49:37 +0100

    Added dataset source to docs

--- R/data.R
+++ R/data.R
@@ -15,11 +15,10 @@
 #' @keywords datasets
 "meb_weights"
 
-
-
 #' JMMI dataset for February 2022
 #'
 #' The cleaned dataset for February 2022 JMMI
+#' @source \url{https://www.impact-repository.org/document/reach/a5964fda/reach_lby_dataset_joint_market_monitoring_initiative_jmmi_February_2022.xlsx}
 #'
 #' @docType data
 #'
