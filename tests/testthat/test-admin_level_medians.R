9fdb2c580cb89f7db825966b76d76a161a606dcf
Iyed GHEDAMSI <iyed.ghedamsi@reach-initiative.org>
2022-11-15 10:55:27 +0100

    tests that admin_level_medians handles computing specific MEB groups

--- tests/testthat/test-admin_level_medians.R
+++ tests/testthat/test-admin_level_medians.R
@@ -4,4 +4,35 @@ test_that("raises error with invalid item group", {
 })
 
 test_that("does not raise error with valid item group", {
+  expect_error(admin_level_medians(jmmi_2022_feb, q_region, "hygiene"), NA)
+})
+
+
+test_that("return all groups when none specified", {
+  all_groups_medians <- admin_level_medians(jmmi_2022_feb, q_region)
+
+  food_medians <- admin_level_medians(jmmi_2022_feb, q_region, "food")
+  food_items <- meb_weights |>
+    dplyr::filter(.data[["group"]] == "food") |>
+    dplyr::pull(.data[["item"]])
+
+  expect_equal(
+    all_groups_medians |> dplyr::filter(.data[["item"]] %in% food_items),
+    food_medians
+  )
+
+  pharmaceutical_medians <- admin_level_medians(jmmi_2022_feb, q_region, "pharmaceutical")
+  pharmaceutical_items <- meb_weights |>
+    dplyr::filter(.data[["group"]] == "pharmaceutical") |>
+    dplyr::pull(.data[["item"]])
+
+  expect_equal(
+    all_groups_medians |> dplyr::filter(.data[["item"]] %in% pharmaceutical_items),
+    pharmaceutical_medians
+  )
+
+  expect_setequal(
+    all_groups_medians |> dplyr::distinct(.data[["item"]]) |> dplyr::pull(.data[["item"]]),
+    meb_weights |> dplyr::distinct(.data[["item"]]) |> dplyr::pull(.data[["item"]])
+  )
 })
