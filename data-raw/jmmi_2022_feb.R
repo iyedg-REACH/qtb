59b864022ef850b819b819bbbf5e65fa99968185
Iyed GHEDAMSI <iyed.ghedamsi@reach-initiative.org>
2022-11-24 17:24:40 +0100

    Added computed MEB costs to internal data

--- data-raw/jmmi_2022_feb.R
+++ data-raw/jmmi_2022_feb.R
@@ -35,12 +35,35 @@ city_medians_cooking_fuel <- get_city_medians(rows = 110:154, cols = 1:4)
 city_medians_pharmaceutical <- get_city_medians(rows = 162:206, cols = 1:6)
 city_medians_gasoline <- get_city_medians(rows = 214:258, cols = 1:3)
 
+meb_costs <- openxlsx::read.xlsx(jmmi_2022_feb_file,
+  sheet = "Cost of MEB",
+  na.strings = c("NA")
+) |>
+  dplyr::as_tibble() |>
+  janitor::clean_names() |>
+  dplyr::rename(
+    q_municipality = city,
+    meb_cost = cost_of_all_meb_key_elements_in_lyd,
+    food_meb_cost = cost_of_food_portion_of_meb_in_lyd,
+    nfi_meb_cost = cost_of_nfi_portion_of_meb_in_lyd,
+    fuel_meb_cost = cost_of_fuel_portion_of_meb_in_lyd
+  ) |>
+  select(
+    q_municipality,
+    meb_cost,
+    food_meb_cost,
+    nfi_meb_cost,
+    fuel_meb_cost
+  ) |>
+  tidyr::pivot_longer(-q_municipality, names_to = "meb", values_to = "cost")
+
 usethis::use_data(
   city_medians_food,
   city_medians_hygiene,
   city_medians_cooking_fuel,
   city_medians_pharmaceutical,
   city_medians_gasoline,
+  meb_costs,
   internal = TRUE,
   overwrite = TRUE
 )
