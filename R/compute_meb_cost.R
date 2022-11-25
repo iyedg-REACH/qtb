a2d09f9f28574fa5d7021e5de6efae059df9a8c3
Iyed GHEDAMSI <iyed.ghedamsi@reach-initiative.org>
2022-11-25 09:09:42 +0100

    Function to compute cost of MEB, not including fuel

---
+++ R/compute_meb_cost.R
@@ -0,0 +1,27 @@
+#' Compute the Cost of the MEB in Libya
+#'
+#' @param df Monthly JMMI clean dataset
+#' @param admin_level_col a column name indicating a Libyan Administrative Level
+#' @param item_group Optional filter to limit the computation to a specific item group in the Minimum Expenditure Basket
+#'
+#' @return A dataframe of the cost of MEB per Administrative Level
+#' @export
+#'
+#' @examples
+#' compute_meb_cost(jmmi_2022_feb, q_region, "food")
+#' compute_meb_cost(jmmi_2022_feb, q_district, "hygiene")
+#' compute_meb_cost(jmmi_2022_feb, q_municipality)
+compute_meb_cost <- function(df, admin_level_col, item_group = NULL) {
+  weighted_df <- admin_level_medians(
+    df,
+    admin_level_col = {{ admin_level_col }},
+    item_group = item_group
+  ) |>
+    dplyr::inner_join(meb_weights, by = "item") |>
+    mutate(weighted_median_item_price = .data[["median_item_price"]] * .data[["weight"]])
+
+  weighted_df |>
+    dplyr::group_by({{ admin_level_col }}) |>
+    dplyr::summarise(meb_cost = sum(.data[["weighted_median_item_price"]], na.rm = TRUE)) |>
+    dplyr::ungroup()
+}
---
+++ tests/testthat/test-compute_meb_cost.R
@@ -0,0 +1,55 @@
+get_expected_item_group <- function(item_group) {
+  if (item_group == "food") {
+    meb_group <- "food_meb_cost"
+  } else if (item_group == "hygiene") {
+    meb_group <- "nfi_meb_cost"
+  } else if (item_group) {
+    meb_group <- "fuel_meb_cost"
+  }
+
+  meb_costs |>
+    dplyr::mutate(q_municipality = stringr::str_to_lower(q_municipality)) |>
+    dplyr::filter(
+      meb == meb_group,
+      q_municipality != "tripoli", # Tripoli is ignore because it is a composite of municipalities
+      stringr::str_detect(
+        q_municipality,
+        "median",
+        negate = TRUE
+      )
+    ) |>
+    dplyr::select(-all_of("meb")) |>
+    dplyr::rename(meb_cost = .data[["cost"]]) |>
+    dplyr::arrange(q_municipality, meb_cost)
+}
+
+prepare_result <- function(df) {
+  df |>
+    dplyr::mutate(q_municipality = stringr::str_to_lower(q_municipality)) |>
+    dplyr::arrange(q_municipality, meb_cost)
+}
+
+test_that("meb computation with an item group matches expected", {
+  expected_food_group <- get_expected_item_group("food")
+
+  food_result <- compute_meb_cost(
+    df = jmmi_2022_feb,
+    admin_level_col = q_municipality,
+    item_group = "food"
+  ) |>
+    prepare_result()
+
+  expect_equal(food_result, expected_food_group)
+
+  expected_hygiene_group <- get_expected_item_group("hygiene")
+
+
+  hygiene_result <- compute_meb_cost(
+    df = jmmi_2022_feb,
+    admin_level_col = q_municipality,
+    item_group = "hygiene"
+  ) |>
+    prepare_result()
+
+  expect_equal(hygiene_result, expected_hygiene_group)
+})
