e4a83b9942be131d43b4ab7ad2c4196a2c2ca4a0
Iyed GHEDAMSI <iyed.ghedamsi@reach-initiative.org>
2022-11-27 17:46:28 +0100

    basic idea for claning log validation

---
+++ R/validate_cleaning_log.R
@@ -0,0 +1,9 @@
+validate_cleaning_log <- function(cleaning_log,
+                                  var_names_col,
+                                  values_col,
+                                  survey,
+                                  tool) {
+  # TODO: test that all variables are valid questions in the tool
+  # TODO: test that for each id-variable combination is unique
+  # TODO: test that for numeric questions, the new value is a valid number or NA
+}
