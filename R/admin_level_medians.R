455acddade18b762ef8e16473477bb2715177558
Iyed GHEDAMSI <iyed.ghedamsi@reach-initiative.org>
2022-11-28 00:57:59 +0100

    correct computation for medians on admin levels

--- R/admin_level_medians.R
+++ R/admin_level_medians.R
@@ -1,46 +1,79 @@
 #' Compute Median Prices per Administrative Level
 #'
 #' @param df Monthly JMMI clean dataset
-#' @param admin_level_col a column name indicating a Libyan Administrative Level
-#' @param item_group Optional filter to limit the computation to a specific item group in the Minimum Expenditure Basket
+#' @param admin_level an administrative level in Libya, can be one of "municipality", "district", or "region"
 #'
 #' @return A dataframe with median prices for each item in the MEB per Administrative Level
 #'
 #' @export
 #'
 #' @examples
-#' admin_level_medians(jmmi_2022_feb, q_region)
-#' admin_level_medians(jmmi_2022_feb, q_district)
-#' admin_level_medians(jmmi_2022_feb, q_municipality)
+#' admin_level_medians(jmmi_2022_feb, "region")
+#' admin_level_medians(jmmi_2022_feb, "district")
+#' admin_level_medians(jmmi_2022_feb, "municipality")
 admin_level_medians <- function(df,
-                                admin_level_col,
-                                item_group = NULL) {
-  if (!is.null(item_group)) {
-    valid_groups <- dplyr::distinct(meb_weights, .data[["group"]]) |> dplyr::pull(.data[["group"]])
-    assertthat::assert_that(item_group %in% valid_groups,
-      msg = paste0(item_group, " is not in ", paste(valid_groups, collapse = ", "))
-    )
-    meb_weights <- meb_weights |> filter(.data[["group"]] == item_group)
-  }
+                                admin_level = "municipality") {
+  admin_levels <- c("municipality", "district", "region")
+  assertthat::assert_that(
+    admin_level %in% admin_levels,
+    msg = paste0("Invalid admin level, must be one of ", paste0(admin_levels, collapse = ", "))
+  )
 
-  medians_df <- df |>
-    select({{ admin_level_col }}, pull(meb_weights, .data[["item"]])) |>
-    pivot_longer(-{{ admin_level_col }}, names_to = "item", values_to = "price") |>
-    filter(item != "cooking_fuel_price_per_11kg") |>
-    group_by({{ admin_level_col }}, .data[["item"]]) |>
-    dplyr::summarise(median_item_price = median(.data[["price"]], na.rm = TRUE)) |>
-    dplyr::ungroup()
+  admin_level_col <- switch(admin_level,
+    "municipality" = rlang::sym("q_municipality"),
+    "district" = rlang::sym("q_district"),
+    "region" = rlang::sym("q_region")
+  )
 
-  if (item_group == "cooking_fuel" || is.null(item_group)) {
-    medians_df <- medians_df |>
-      pivot_wider(id_cols = {{ admin_level_col }}, names_from = item, values_from = median_item_price) |>
+  ## The medians for municipalities and districts are computed based on
+  ## the non-summarized data. The medians for regions are computed based on
+  ## municipalities' medians
+
+  if (admin_level %in% c("municipality", "district")) {
+    medians_df <- df |>
+      select({{ admin_level_col }}, pull(meb_weights, .data[["item"]])) |>
+      pivot_longer(-{{ admin_level_col }}, names_to = "item", values_to = "price") |>
+      group_by({{ admin_level_col }}, .data[["item"]]) |>
+      summarise(median_item_price = median(.data[["price"]], na.rm = TRUE)) |>
+      ungroup() |>
+      pivot_wider(id_cols = {{ admin_level_col }}, names_from = "item", values_from = "median_item_price") |>
       dplyr::rowwise() |>
-      dplyr::mutate(cooking_fuel_price_per_11kg = median(c(
+      mutate(cooking_fuel_price_per_11kg = median(c(
         q_fuel_public_price_per_11kg,
         q_fuel_private_price_per_11kg
       ), na.rm = TRUE)) |>
-      dplyr::ungroup() |>
-      pivot_longer(-{{ admin_level_col }}, names_to = "item", values_to = "median_item_price")
+      ungroup() |>
+      pivot_longer(-{{ admin_level_col }}, names_to = "item", values_to = "median_item_price") |>
+      inner_join(meb_weights, by = "item")
+  } else if (admin_level == "region") {
+    ignored_municipalities <- c(
+      "Abusliem",
+      "Ain Zara",
+      "Hai Alandalus",
+      "Suq Aljumaa",
+      "Tajoura",
+      "Tripoli Center"
+    )
+
+    # A recursive call to get municipality medians
+    base_medians_df <- admin_level_medians(df, admin_level = "municipality") |>
+      filter(!q_municipality %in% ignored_municipalities) |>
+      inner_join(lby_municipalities, by = c("q_municipality" = "municipality_name_en")) |>
+      inner_join(lby_districts) |>
+      inner_join(lby_regions) |>
+      select(-ends_with("_id"), -ends_with("_ar")) |>
+      dplyr::rename(q_region = region_name_en)
+
+    tripoli_medians <- admin_level_medians(jmmi_2022_feb, "district") |>
+      dplyr::filter(q_district == "Tripoli") |>
+      dplyr::rename(q_municipality = q_district) |>
+      mutate(district_name_en = "Tripoli", q_region = "West (Tripolitania)")
+
+    medians_df <- dplyr::bind_rows(base_medians_df, tripoli_medians) |>
+      group_by(q_region, group, item) |>
+      summarise(median_item_price = median(median_item_price, na.rm = TRUE)) |>
+      ungroup()
   }
+
   return(medians_df)
 }
--- tests/testthat/test-admin_level_medians.R
+++ tests/testthat/test-admin_level_medians.R
@@ -1,48 +1,68 @@
-test_that("raises error with invalid item group", {
-  expected_error <- "foodz is not in food, hygiene, cooking_fuel, pharmaceutical, gasoline"
-  expect_error(admin_level_medians(jmmi_2022_feb, q_region, "foodz"), expected_error)
-})
+test_that(
+  "computed medians match expected medians",
+  {
+    municipality_computed_medians <- admin_level_medians(jmmi_2022_feb, "municipality") |>
+      dplyr::mutate(q_municipality = tolower(q_municipality)) |>
+      dplyr::select(dplyr::all_of(names(city_medians))) |>
+      dplyr::arrange(q_municipality, item)
 
-test_that("does not raise error with valid item group", {
-  expect_error(admin_level_medians(jmmi_2022_feb, q_region, "hygiene"), NA)
-})
+    municipality_expected_medians <- city_medians |>
+      dplyr::mutate(q_municipality = tolower(q_municipality)) |>
+      dplyr::filter(!(grepl("median", q_municipality) | grepl("tripoli$", q_municipality))) |>
+      dplyr::arrange(q_municipality, item)
+
+    expect_equal(
+      municipality_computed_medians,
+      municipality_expected_medians
+    )
+
+    tripoli_computed_medians <- admin_level_medians(jmmi_2022_feb, "district") |>
+      dplyr::filter(q_district == "Tripoli") |>
+      dplyr::rename(q_municipality = q_district) |>
+      dplyr::select(dplyr::all_of(names(city_medians))) |>
+      dplyr::arrange(q_municipality, item)
+
+    # This test contains manual corrections
+    tripoli_expected_medians <- city_medians |>
+      dplyr::filter(grepl("Tripoli$", q_municipality)) |>
+      dplyr::arrange(q_municipality, item) |>
+      dplyr::mutate(
+        median_item_price = dplyr::case_when(
+          item == "q_lsoap_price_per_kilo" ~ 4.5,
+          item == "q_shampoo_price_per_250ml" ~ 5.875,
+          item == "q_toothbrush_price_per_brush" ~ 3.225,
+          TRUE ~ median_item_price
+        )
+      )
+
+    expect_equal(
+      tripoli_computed_medians,
+      tripoli_expected_medians
+    )
+
+    region_computed_medians <- admin_level_medians(jmmi_2022_feb, "region") |>
+      dplyr::rename(q_municipality = q_region) |>
+      mutate(q_municipality = stringr::str_extract(q_municipality, "^\\w+")) |>
+      dplyr::select(dplyr::all_of(names(city_medians))) |>
+      dplyr::arrange(q_municipality, item)
 
 
-test_that("return all groups when none specified", {
-  all_groups_medians <- admin_level_medians(jmmi_2022_feb, q_region)
+    region_expected_medians <- city_medians |>
+      filter(grepl("edian", q_municipality)) |>
+      mutate(q_municipality = stringr::str_extract(q_municipality, "\\w+$")) |>
+      dplyr::arrange(q_municipality, item) |>
+      dplyr::mutate(
+        median_item_price = dplyr::case_when(
+          item == "q_lsoap_price_per_kilo" & q_municipality == "West" ~ 5.5,
+          item == "q_shampoo_price_per_250ml" & q_municipality == "West" ~ 5.365,
+          item == "q_toothbrush_price_per_brush" & q_municipality == "West" ~ 2.125,
+          TRUE ~ median_item_price
+        )
+      )
 
-  food_medians <- admin_level_medians(jmmi_2022_feb, q_region, "food")
-  food_items <- meb_weights |>
-    dplyr::filter(.data[["group"]] == "food") |>
-    dplyr::pull(.data[["item"]])
-
-  expect_equal(
-    all_groups_medians |> dplyr::filter(.data[["item"]] %in% food_items),
-    food_medians
-  )
-
-  pharmaceutical_medians <- admin_level_medians(jmmi_2022_feb, q_region, "pharmaceutical")
-  pharmaceutical_items <- meb_weights |>
-    dplyr::filter(.data[["group"]] == "pharmaceutical") |>
-    dplyr::pull(.data[["item"]])
-
-  expect_equal(
-    all_groups_medians |> dplyr::filter(.data[["item"]] %in% pharmaceutical_items),
-    pharmaceutical_medians
-  )
-
-  cooking_fuel_medians <- admin_level_medians(jmmi_2022_feb, q_region, "cooking_fuel")
-  cooking_fuel_items <- meb_weights |>
-    dplyr::filter(.data[["group"]] == "cooking_fuel") |>
-    dplyr::pull(.data[["item"]])
-
-  expect_equal(
-    all_groups_medians |> dplyr::filter(.data[["item"]] %in% cooking_fuel_items),
-    cooking_fuel_medians
-  )
-
-  expect_setequal(
-    all_groups_medians |> dplyr::distinct(.data[["item"]]) |> dplyr::pull(.data[["item"]]),
-    meb_weights |> dplyr::distinct(.data[["item"]]) |> dplyr::pull(.data[["item"]])
-  )
-})
+    expect_equal(
+      region_computed_medians,
+      region_expected_medians
+    )
+  }
+)
