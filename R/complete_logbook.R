98c9447d4c265dbeb3264b84a363756cf6e48062
Iyed GHEDAMSI <iyed.ghedamsi@reach-initiative.org>
2023-02-20 08:34:21 +0100

    function to complete logbook data

---
+++ R/complete_logbook.R
@@ -0,0 +1,44 @@
+complete_logbook <- function(raw_data, logbook) {
+  necessary_columns <- c(
+    "uuid",
+    "question.name",
+    "Issue",
+    "Type of Issue",
+    "feedback",
+    "old.value",
+    "new.value"
+  )
+
+
+  necessary_logbook <- dplyr::select(
+    logbook,
+    dplyr::all_of(necessary_columns)
+  )
+
+  completion_columns <- c(
+    "_uuid",
+    "deviceid",
+    "q_name"
+  )
+
+  completion_df <- dplyr::select(
+    raw_data,
+    dplyr::all_of(completion_columns)
+  ) |>
+    dplyr::rename(
+      `device ID` = deviceid,
+      `Enumerator ID` = q_name
+    )
+
+  tidylog::left_join(necessary_logbook, completion_df, by = c("uuid" = "_uuid")) |>
+    dplyr::select(
+      "uuid",
+      "Enumerator ID",
+      "device ID",
+      "question.name",
+      "Issue",
+      "Type of Issue",
+      "feedback",
+      "old.value", "new.value"
+    )
+}
